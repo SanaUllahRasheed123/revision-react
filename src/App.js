6c76e6bf2e5af688e25ec6db0a828040c6925cf7
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-22 12:04:09 +0500

    useRef and pracitise of it

--- src/App.js
+++ src/App.js
@@ -1,29 +1,101 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-// import Header from './Header';
+// import { useEffect, useState } from 'react';
+// import './App.css';
+// // import Header from './Header';
 
-function App() {
-  const [count , setCount] = useState(false);
-  const [data,setData] = useState(50)
+// function App() {
+//   const [count , setCount] = useState(false);
+//   const [data,setData] = useState(50)
 
-  useEffect( ()=>{
-    console.log("I'm from useEffect Hook")
-  },[])
+//   useEffect( ()=>{
+//     console.log("I'm from useEffect Hook")
+//   },[])
 
-  function updateData(){
-    setData(data-1)
+//   function updateData(){
+//     setData(data-1)
+//   }
+//   return (
+
+//     <>
+//     <h1>The count is {count}</h1>
+// <button onClick={()=>{setCount(count+1)}}>+  {count}
+// </button>
+// <button onClick={updateData}>The value is now {data}</button>
+// {/* <Header name="Sana Ullah"/> */}
+//     </>
+   
+//   );
+// }
+
+// export default App;
+
+
+
+// // import React, { useRef, useState } from 'react'
+
+// // const App = () => {
+// //   const refElement = useRef("")
+// //   const [name,setName] = useState("SanaUllah")
+
+// //   // console.log(refElement);
+// // function Reset(){
+// //   setName("")
+// //   refElement.current.focus();
+// //   refElement.current.style.color = "green";
+// // }
+
+
+// // function updateInput(){
+// //   refElement.current.style.color = "red";
+// //   refElement.current.style.fontSize = "50px"
+// //   refElement.current.style.padding = "20px"
+// //   refElement.current.style.margin = "10px"
+// //   // refElement.current.style.borderStyle = "outset"
+// //   // refElement.current.style.borderWidth = "20px"
+// //   // refElement.current.style.borderColor = "blue"
+// //   refElement.current.style.border = "10px outset blue"
+// //   refElement.current.style.textAlign = "center"
+
+// // }
+// //   return (
+
+// //     <>
+// //       <input ref={refElement} type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+// //       <button onClick={Reset}>Reset</button>
+// //       <button onClick={updateInput}>Click here to change the color and size</button>
+// //     </>
+// //   )
+// // }
+
+// export default App
+
+
+
+import React, { useRef, useState } from 'react'
+
+const App = () => {
+  const refElement = useRef("")
+  const [name , setName] = useState("Sana Ullah")
+
+  function resetData(){
+    setName("")
+    refElement.current.focus();
+
+  }
+
+  function handleInput(){
+    refElement.current.value = "Congratulations for Successful Registration"
+    refElement.current.style.color = "Green"
+
+    refElement.current.style.fontSize = "50px"
   }
   return (
-
     <>
-    <h1>The count is {count}</h1>
-<button onClick={()=>{setCount(count+1)}}>+  {count}
-</button>
-<button onClick={updateData}>The value is now {data}</button>
-{/* <Header name="Sana Ullah"/> */}
-    </>
-   
-  );
+    <h1>Learning UseReferene Hook</h1>
+    <input ref={refElement} type='text' value={name} onChange={(e)=>{setName(e.target.value)}}/><br/>
+    <button onClick={resetData} >Reset your text now</button>
+    <button onClick={handleInput}>All the Styling will be apply after clicking this button</button>
+     </>
+  )
 }
 
-export default App;
+export default App
