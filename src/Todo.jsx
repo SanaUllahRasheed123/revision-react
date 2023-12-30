c05aa07db6e3a46414877913649c3833bed9b86c
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-30 20:28:43 +0500

    todo list done

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,9 +5,27 @@ const Todo = () => {
   const [listData,setListData] = useState([])
 
   function addActivity(){
-    setListData([...listData, activity])
-    console.log(listData)
+    // setListData([...listData, activity])
+    // console.log(listData)
+    setListData((listData)=>{
+      const updatedList = [...listData,activity]
+      console.log(updatedList)
+      setActivity("")
 
+      return updatedList;
+    })
+
+  }
+
+  function removeActivity(i){
+     const updatedListData = listData.filter((elem,id)=>{
+      return i!=id;
+     })
+     setListData(updatedListData)
+     
+  }
+  function removeAll(){
+    setListData([])
   }
   return (
     <>
@@ -15,6 +33,23 @@ const Todo = () => {
         <div className="header">TodoList</div>
         <input type="text" placeholder="Add activity" value={activity} onChange={(e)=>{setActivity(e.target.value)}}/>
         <button onClick={addActivity}>Add</button>
+        <p className="List-heading">Here is your List {")"}</p>
+        
+        {listData!= [] && listData.map((data,i)=>{
+          return(
+            <>
+              <p key={i}>
+                  <div className="listData">{data}</div>
+                  <div className="btn-position" onClick={()=>removeActivity(i)}><button>remove(-)</button></div>
+              </p>
+            </>
+          )
+        })}
+
+        {listData.length>=1 &&
+          <button onClick={removeAll}>Remove all</button>
+        }
+
 
       </div>
     </>
