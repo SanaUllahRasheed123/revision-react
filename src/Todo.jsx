12c8e1b25cdbb267efe27248056da5d2d79f01f0
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-30 18:10:27 +0500

    working on listData

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './Todo.css'
+import React, { useState } from "react";
+import "./Todo.css";
 const Todo = () => {
+  const [activity, setActivity] = useState("")
+  const [listData,setListData] = useState([])
+
+  function addActivity(){
+    setListData([...listData, activity])
+    console.log(listData)
+
+  }
   return (
     <>
+      <div className="container">
+        <div className="header">TodoList</div>
+        <input type="text" placeholder="Add activity" value={activity} onChange={(e)=>{setActivity(e.target.value)}}/>
+        <button onClick={addActivity}>Add</button>
 
-    
-    <div className='container'>
-    <div className='header'>
-      TodoList
-
-    </div>
-    </div>
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
