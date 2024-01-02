fec57d3ecca6d24ce5307c8addf9ab482f0f36ff
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-01-02 23:40:46 +0500

    Conditonal rendering Ternary Operator Done

--- src/AgeValidation.jsx
+++ src/AgeValidation.jsx
@@ -19,28 +19,28 @@ const AgeValidation = () => {
 
   // ************************ Variable Element condtion**********************
 
-  let age;
-  if(isAgeValid){
-     age = <div>U can vote</div>
-  }
-  else{
-    age= <h1>You cannot vote</h1>
-  }
-  return(
-   <>
-     {age} 
-   </>
-  )
+  // let age;
+  // if(isAgeValid){
+  //    age = <div>U can vote</div>
+  // }
+  // else{
+  //   age= <h1>You cannot vote</h1>
+  // }
+  // return(
+  //  <>
+  //    {age} 
+  //  </>
+  // )
 
 
   // *********************** Ternary Operator condtion**********************
 
 
-// return(
-//     <>
-//         {isAgeValid? <div>you can vote</div>: <div>You canot vote</div>}
-//     </>
-// )
+return(
+    <>
+        {isAgeValid? <div>you can vote</div>: <div>You canot vote</div>}
+    </>
+)
 
   // ************************ Short Circuit Operator**********************
   //short circuit operator use only when we have to display only true condition, it not works 
